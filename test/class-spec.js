615960b460ea1a4dfbac58f6d523cec61443fce6
xgg0213 <m.eringan@gmail.com>
2024-05-01 22:40:55 -0700

    solutions

--- test/class-spec.js
+++ test/class-spec.js
@@ -4,31 +4,38 @@ const expect = chai.expect;
 const { Word } = require("../class");
 
 describe("Word", function () {
+  let word = new Word('terminal');
+
   describe("Word constructor function", function () {
-    it('should have a "word" property', function () {
-      expect.fail("replace with your code");
+    it('should have a "word" property', function () { // check if word.word exists
+      expect(word.word).to.exist//to.equal('terminal');
+      //expect.fail(word.word); // why having this line fails the test spec?
     });
   
     it('should set the "word" property when a new word is created', function () {
-      expect.fail("replace with your code");
+      expect(word.word).to.equal('terminal');
+      //expect.fail(word.word, true);
     });
   });
 
   describe("removeVowels function", function () {
     it("should return a the word with all vowels removed", function () {
-      expect.fail("replace with your code");
+      expect(word.removeVowels()).to.equal('trmnl')
+      //expect.fail(word.removeVowels(),'trmnl');
     });
   });
 
   describe("removeConsonants function", function () {
     it("should return the word with the consonants removed", function () {
-      expect.fail("replace with your code");
+      expect(word.removeConsonants()).to.equal('eia')
+      //expect.fail(word.removeConsonants(),'eia');
     });
   });
   
   describe("pigLatin function", function () {
     it("should return the word converted to pig latin", function () {
-      expect.fail("replace with your code");
+      expect(word.pigLatin()).to.equal('erminaltay');
+      //expect.fail(word.pigLatin(), 'trminalay');
     });
   });
 });
